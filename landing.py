8c3372d50053df5b14928b7eccc0b22ce74798ef
Jonathan Rudman <jonathan.rudman@live.co.uk>
2023-12-23 17:32:27 +0000

    Folder selection dialog

--- landing.py
+++ landing.py
@@ -6,14 +6,22 @@ from PySide6 import QtWidgets, QtGui, QtCore
 class Landing(QtWidgets.QWidget):
     layout: Callable[..., QtWidgets.QLayout] | QtWidgets.QLayout
     placeholder = None
+    confirm_sig = QtCore.Signal(str)
 
     def __init__(self, parent=None):
         super(Landing, self).__init__(parent)
         self.layout = QtWidgets.QVBoxLayout(self)
 
         self._dir_select_layout = QtWidgets.QHBoxLayout()
-        self._dir_line_edit = QtWidgets.QLineEdit()
+        self._dir_line_edit = QtWidgets.QLineEdit(
+            QtCore.QStandardPaths.standardLocations(
+                QtCore.QStandardPaths.StandardLocation.HomeLocation
+            )[0]
+        )
+        self._dir_line_edit.setFixedWidth(300)
+
         self._dir_select_button = QtWidgets.QPushButton("Select folder")
+        self._dir_select_button.clicked.connect(self._dir_select_dialog)
         self._dir_select_layout.addWidget(self._dir_line_edit)
         self._dir_select_layout.addWidget(self._dir_select_button)
 
@@ -26,4 +34,11 @@ class Landing(QtWidgets.QWidget):
         self.layout.addLayout(self._dir_select_layout)
         self.layout.addWidget(self._show_comp_button)
         self.layout.addWidget(self._show_raw_button)
-        self.layout.addWidget(self.confirm_button)
+        self.layout.addWidget(
+            self.confirm_button, alignment=QtCore.Qt.AlignmentFlag.AlignCenter
+        )
+
+    def _dir_select_dialog(self):
+        dialog = QtWidgets.QFileDialog(self)
+        folder = dialog.getExistingDirectory()
+        self._dir_line_edit.setText(folder)
